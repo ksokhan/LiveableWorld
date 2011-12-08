f195d2d3757123310385f30081824403d4df4cca
Konstantin Sokhan <k@ksokhan.com>
2011-12-08 14:01:21 -0500

    Major work: using mongoskin as db and revamped routing. also added initial google maps

--- app.js
+++ app.js
@@ -4,21 +4,22 @@
  */
 
 var express = require('express'), 
-    routes = require('./routes');
+    routes = require('./routes'),
+	mongo = require('mongoskin');
 
-var app = module.exports = express.createServer();
+var app = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(express.logger('dev'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/libs'));
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
@@ -28,10 +29,21 @@ app.configure('development', function(){
   app.use(express.errorHandler()); 
 });*/
 
+
+// DB setup
+db = mongo.db('localhost:27017/liveableworld');
+places = db.collection('Places');
+
 // Routes
 
 app.get('/', routes.index);
-//app.get('/about', res.send('hi'));
+app.get('/discover', routes.search);
+app.get('/browse', routes.browse);
+
+// data
+app.get('/discover/init', routes.database_init);
+app.get('/cleardb', routes.database_clear);
+app.get('/data', routes.database_test);
 
 app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,47 @@
  */
 
 exports.index = function(req, res){
-
+	
   	res.render('home', { 
 		header_class: 'big',
 		header_title: 'Liveable World',
 		header_description: 'Find the best place to live; Liveable World is an experimental index of the most liveable places on earth through realtime data and collective statistics. Its a new way to examine our world.'
 	});
+};
 
+exports.browse = function(req, res){
+  	res.render('browse', { 
+		header_class: 'normal border',
+		header_title: 'Liveable World: Browse data',
+		layout: 'layouts/browse'
+	});
+};
+
+exports.search = function(req, res){
+  	res.render('search', { 
+		header_class: 'big',
+		header_title: 'Liveable World',
+		header_description: 'Fill out the details below according to how important you feel each category for a place to live. And some additional text to describe the process of filling out the form.',
+		layout: 'layouts/search'
+	});		
+};
+
+exports.database_init = function(req, res){
+  	// bootstrap with sample
+	places.save({ name: "Toronto", num: 25 });
+	places.save({ name: "Calgary", num: 5 });
+	places.save({ name: "Ottawa", num: 7 });
+	places.save({ name: "New York", num: 33 });
+	res.send('done');
+};
+
+exports.database_test = function(req, res){
+  	places.find().toArray(function(err, items){
+		console.log(items);
+	    res.send(items, { 'Content-Type': 'text/plain' });
+	});
+};
+
+exports.database_clear = function(req, res){
+  	places.remove();
 };
